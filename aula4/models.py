dcad91ddcc179b89f443af7d36f4149d9b6b0e9f
macOS <macos@macOSs-MacBook-Air.local>
2025-09-16 10:40:02 -0300

    up

--- aula4/models.py
+++ aula4/models.py
@@ -6,6 +6,7 @@ class Usuario(db.Model):
     nome = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     data_de_nascimento = db.Column(db.Date, nullable=True)  # Nova coluna
+    data_de_aniversario = db.Column(db.Date, nullable=True)
 
     def __repr__(self):
         return f'<Usuario {self.nome}>'
